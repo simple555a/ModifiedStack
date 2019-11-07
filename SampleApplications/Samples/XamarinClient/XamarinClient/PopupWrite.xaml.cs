5e2ee5f727812eb0290d6832805a94056db6558d
Rakshan Premsagar Kapikad <rakshan.kapikad@uni-rostock.de>
2019-11-07 14:12:32 +0100

    first commit

---
+++ SampleApplications/Samples/XamarinClient/XamarinClient/PopupWrite.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinClient
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+public partial class PopupWrite : ContentPage
+{
+    public PopupWrite()
+    {
+        InitializeComponent();
+    }
+}
+}
